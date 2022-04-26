d03c928b89b65976f91f47b0e9a1aac2835db0b3
Conner Bachmann <59223342+Intevel@users.noreply.github.com>
2022-04-26 19:08:31 +0200

    fix(main): removed unnecessary logs

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,3 @@
 import { defineNuxtPlugin } from "#app";
 
-export default defineNuxtPlugin((nuxtApp) => {
-  console.log("[nuxt-logsnag]: Plugin loaded!");
-});
+export default defineNuxtPlugin((nuxtApp) => {});
